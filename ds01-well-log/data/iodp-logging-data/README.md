0c25d3d17b9ded22fbb50cee58a44fc1cafd76ef
Ryan Du <ryan.du@mines.edu>
2026-02-02 16:45:27 -0700

    resolve conflict

---
+++ ds01-well-log/data/iodp-logging-data/README.md
@@ -0,0 +1,6 @@
+## General information
+last modified: 2026-01-26
+data created: 2026-01-26
+author: Ryan Du
+## Dateset information
+- General Description: files containing well-logging data from the International Ocean Drilling Program (IODP)
---
+++ short-assignment/python-intro.py
@@ -0,0 +1,9 @@
+# Learning how to write a Python script - GPGN268
+# Author: Bia Villas Boas
+# Date: Jan 31, 2023
+
+# Temperature in degrees Fahrenheit
+temperature = 55
+# Location of the measurement
+city = "Golden"
+print("It is", temperature, "F", "in", city)
